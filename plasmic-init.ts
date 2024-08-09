7522a7bbb1108810542477919b2b9750df1656f3
Karo <obkaro@gmail.com>
2024-08-09 12:53:14 -0400

    chore: Update Plasmic configuration to use

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -7,9 +7,9 @@ export const PLASMIC = initPlasmicLoader({
       token:
         "zmo8HNSEF2XemY87OxDC203OvEB2cUJIWSL5YxMJn3fu0KVtHiTARKRNlUWuCGueW9WVzrxn6P53hGjPksyg",
       version:
-        process.env.NODE_ENV === "production"
+        process.env.NEXT_PUBLIC_NODE_ENV === "production"
           ? "prod"
-          : process.env.NODE_ENV === "development"
+          : process.env.NEXT_PUBLIC_NODE_ENV === "development"
           ? "staging"
           : // If not production or staging, then just use the latest
             // published version, regardless of tags
